3b7a5172b83bc048fe40c26c8eda69fc53856af2
sneha02 <snehamane021997@gmail.com>
2017-08-27 16:52:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req,res){
-   res.send('article-one is requested'); 
+ res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
