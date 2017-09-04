03b506756f7624b4f2ebfc4adb585c798227fff0
sneha02 <snehamane021997@gmail.com>
2017-09-04 17:56:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,6 +5,12 @@ element.innerHTML= 'new value';
 //move the image
 
 var img = document.getElementById('madi');
+var marginLeft=0;
+function moveRight(){
+    marginLeft=marginLeft +10;
+    img.style.marginLeft=maarginLeft + 'px';
+}
 img.onclick=function(){
-    img.style.marginLeft='100px';
+    var interval = setInterval(moveRight,100);
+
 }
