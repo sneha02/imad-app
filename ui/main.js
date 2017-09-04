2d8cd64c962ada7d4d0edafac14582101b2f2286
sneha02 <snehamane021997@gmail.com>
2017-09-04 17:53:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,10 @@
 console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML= 'new value';
-var img=document.getElementById('madi');
-img.onlick=function(){
+
+//move the image
+
+var img = document.getElementById('madi');
+img.onclick=function(){
     img.style.marginLeft='100px';
 }
